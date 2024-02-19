96e6812abc7cc9fc542accc1832e764895c0ee53
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-02-19 18:12:43 +0530

    Blog Search Extension Completed

---
+++ extension.js
@@ -0,0 +1,45 @@
+const vscode = require("vscode");
+const axios = require("axios");
+const { XMLParser } = require("fast-xml-parser");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+// bf74skqz4gknqvgcv5qdv6htzvwsfoqdo52cjtucjtqqvmhotuga
+
+async function activate(context) {
+	const res = await axios.get("https://blog.webdevsimplified.com/rss.xml");
+	const parser = new XMLParser();
+	const articles = parser.parse(res.data).rss.channel.item.map((article) => {
+		return {
+			label: article.title,
+			detail: article.description,
+			link: article.link
+		};
+	});
+
+	let disposable = vscode.commands.registerCommand(
+		"search-blog-example-js.onlyblogs",
+		async function () {
+			const article = await vscode.window.showQuickPick(articles, {
+				matchOnDetail: true,
+			});
+			if(article == null) return;
+
+			vscode.env.openExternal(article.link);
+
+			console.log(article);
+		}
+	);
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate,
+};
